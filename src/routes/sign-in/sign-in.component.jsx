6f59f6872306fa1a5c3258dbdb1614c0037d4dca
srijaaan <srijanchoudhury4@gmail.com>
2024-02-28 16:32:09 +0530

    added firebase signin with google popup and rediirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,16 @@
-import {signInWithGooglePopup, createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils'
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+import {auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect} from '../../utils/firebase/firebase.utils'
 const SignIn = () => {
+  useEffect(()=>{
+    const getRes = async () =>{
+      const response = await getRedirectResult(auth)
+      if(response){
+        const userDocRef = await createUserDocumentFromAuth(response.user)
+      }
+    }
+    getRes()
+  },[])
   const logGoogleUser = async()=>{
     const {user} = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user)
@@ -9,6 +20,7 @@ const SignIn = () => {
     <>
       <div>SignIn</div>
       <button onClick={logGoogleUser}>Sign In With Google</button>
+      <button onClick={signInWithGoogleRedirect}>Sign In With Google Redirect</button>
     </>
   )
 }
