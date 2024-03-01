e8d3b1cfc326cef31766c3bccd65ef0265971d0a
srijaaan <srijanchoudhury4@gmail.com>
2024-03-01 13:49:00 +0530

    need to fix the classname conflict using styled components

---
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,18 @@
+import CategoryPreview from "../../components/category-preview/category-preview.component";
+import { CategoriesContext } from "../../contexts/categories.context";
+import { useContext } from "react";
+
+const CategoriesPreview = () => {
+  const {categoriesMap} = useContext(CategoriesContext);
+  return (
+    <>
+      {
+        Object.keys(categoriesMap).map((title)=>{
+          const products =  categoriesMap[title]
+          return <CategoryPreview key={title} title={title} products={products}/>
+        })}
+    </>
+  );
+};
+
+export default CategoriesPreview;
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,24 +1,14 @@
-import ProductCard from "../../components/product-card/product-card.component";
-import { CategoriesContext } from "../../contexts/categories.context";
-import { useContext } from "react";
+import { Route, Routes } from 'react-router-dom';
 import './shop.styles.scss'
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
 
 const Shop = () => {
-  const {categoriesMap} = useContext(CategoriesContext);
   return (
-    <>
-      {
-        Object.keys(categoriesMap).map((title)=>(
-          < div key={title}>
-            <h2>{title.toUpperCase()}</h2>
-            <div className="products-container">
-              {categoriesMap[title].map((product) => (
-                <ProductCard key={product.id} product={product}/>
-              ))}
-            </div>
-          </div>
-        ))}
-    </>
+    <Routes>
+      <Route index element={<CategoriesPreview/>}/>
+      <Route path=':category' element={<Category/>}/>
+    </Routes>
   );
 };
 
